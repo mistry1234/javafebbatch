57422f6ba2abb7f67de90cf6309f7996c89d71a3
mistry1234 <41130923+mistry1234@users.noreply.github.com>
2022-04-03 18:57:46 -0700

    Update DemoSb.java
    created and updated file

--- src/StringBuffer/DemoSb.java
+++ src/StringBuffer/DemoSb.java
@@ -6,4 +6,36 @@ package StringBuffer;
 //    s.concat("in java");
 //    System.out.print(s);
 
+char ch[]= {'j','a','v','a'};
+		String s= new String(ch);
+		System.out.print(s);
+		StringBuffer sb=new StringBuffer("this is java program");
+		String s2= new String(sb);
+		System.out.print(s2);
+		
+		StringBuffer sb1=new StringBuffer("this is java program");
+		
+		
+		
+	}
+
+
+
+//    String s="string object";
+//    s.concat("in java");
+//    System.out.print(s);
+
+//      StringBuffer sb=new StringBuffer("this is java program");
+//      //System.out.print(sb);
+//      sb.append("for String buffer");
+//      System.out.print(sb);
+}     
+
+
+
+
+
+
+      
+
 
