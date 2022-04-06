c392402015eaafd130e5cbc0ede32cfc92df272c
mistry1234 <41130923+mistry1234@users.noreply.github.com>
2022-04-06 13:15:29 -0700

    Update DemoSb.java
    change error for merge issue

--- src/StringBuffer/DemoSb.java
+++ src/StringBuffer/DemoSb.java
@@ -25,18 +25,7 @@ public class DemoSb {
 //    s.concat("in java");
 //    System.out.print(s);
 
-<<<<<<< HEAD
-=======
-char ch[]= {'j','a','v','a'};
-		String s= new String(ch);
-		System.out.print(s);
-		StringBuffer sb=new StringBuffer("this is java program");
-		String s2= new String(sb);
-		System.out.print(s2);
-		
-		StringBuffer sb1=new StringBuffer("this is java program");
-		
-		
+
 		
 	}
 
@@ -46,12 +35,6 @@ char ch[]= {'j','a','v','a'};
 //    s.concat("in java");
 //    System.out.print(s);
 
->>>>>>> 57422f6ba2abb7f67de90cf6309f7996c89d71a3
-//      StringBuffer sb=new StringBuffer("this is java program");
-//      //System.out.print(sb);
-//      sb.append("for String buffer");
-//      System.out.print(sb);
-}     
 
 
 
